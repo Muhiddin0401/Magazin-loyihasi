a3632525ccd04f322e2973756b32a4aa74dca1cf
nurullo <choriyevnurullo0329@gmail.com>
2024-12-16 20:25:08 +0500

    user malumotlari

---
+++ dars.py
@@ -0,0 +1,14 @@
+# clas user
+#             tel,kartasi,karzinka,balans,passwort,rol
+#
+#
+#
+#
+# clas mahsulotlar
+#             nomi ,narhi, mudati
+#
+#
+#
+# clas dokon
+#             nomi,bazasi_mahsulotlar,balans,baza_istoriya
+
